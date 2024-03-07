199766f7df82e738c4027991afb24ad95d5af0eb
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-03-07 22:07:12 +0500

    onClick

--- src/app/layout/components/Nav.jsx
+++ src/app/layout/components/Nav.jsx
@@ -3,22 +3,26 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 function Nav(props) {
   const pathName = usePathname();
 
+  const handleNavigation = (href) => {
+    // Server tomonidan yuklash uchun client tomonidan navigatsiyani boshlaymiz
+    router.push(href);
+  };
+
   return (
     <ul className=" flex justify-between items-center gap-[61px] table1005:gap-[30px] table945:flex table945:flex-col  ">
       <li className=" py-[3px] font-semibold text-14  hover:text-primary">
-        <Link  href="/" className={`${pathName === "/" ? "active" : ""}`}>
+        <Link onClick={() => handleNavigation("/")}  className={`${pathName === "/" ? "active" : ""}`}>
           Home
         </Link>
       </li>
       <li className=" py-[3px] font-semibold text-14  hover:text-primary">
         <Link
-          href="/clothes"
+        onClick={() => handleNavigation("/clothes")}
           className={`${pathName === "/clothes" ? "active" : ""}`}
         >
           Clothes
